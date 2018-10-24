a4f11567dc305345d6e6e967fdb3076e0408cef8
gaoyong <gaoyong@wucar.com.cn>
2018-10-24 19:29:53 +0800

    spring with hibernate

--- hhdemo/src/springDemo/Test/Case1.java
+++ hhdemo/src/springDemo/Test/Case1.java
@@ -2,18 +2,53 @@ package springDemo.Test;
 
 
 import org.springframework.context.ApplicationContext;
+
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import springDemo.User;  
+
+import org.springframework.orm.hibernate5.HibernateTemplate;
+
+import org.hibernate.Session;
+
+import org.hibernate.Transaction;
 
 public class Case1 {
 
 	public static void main(String[] args) {
 
-
 		ApplicationContext context =new ClassPathXmlApplicationContext("appContextxml.xml");
+
 		User user = (User)context.getBean("user");
 		System.out.println(user);
+		
+		try
+		{
+			ApplicationContext context1 =new ClassPathXmlApplicationContext("appContextxml2.xml");
+			
+			HibernateTemplate ht = (HibernateTemplate) context1.getBean("hibernateTemplate");
+			
+			Session ses = ht.getSessionFactory().openSession();
+			
+			
+			
+			hd.User adduser = new hd.User("Sky11","333","409169399@qq.com","xiamen_sh");
+			
+			Transaction trans = ses.beginTransaction();
+			
+			ses.save(adduser);
+			
+			trans.commit();
+			
+			ses.close();
+			
+			System.out.print("ok");
+			
 		}
+		catch (Exception exp)
+		{
+			exp.printStackTrace();
+		}
+	}
 	
 
 	
